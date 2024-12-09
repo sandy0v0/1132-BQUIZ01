8af8ba555a1413ef165b98be1de86ab545e3c4b9
sandy0v0 <be0324ss@gmail.com>
2024-12-09 16:38:37 +0800

    完成編輯次選單功能

--- backend/menu.php
+++ backend/menu.php
@@ -16,43 +16,43 @@
         <form method="post" action="./api/edit.php">
             <table width="100%">
                 <tbody>
-                    <tr class="yel">                      
+                    <tr class="yel">
                         <td width="30%">主選單名稱</td>
-                        <td width="30%">選單連結網址</td>
+                        <td width="30%">選單連結網址：</td>
                         <td width="10%">次選單數</td>
                         <td width="10%">顯示</td>
                         <td width="10%">刪除</td>
                         <td></td>
                     </tr>
                     <?php
-                    $rows=$Menu->all();
-                    foreach($rows as $row){           
-
+                    $rows=$Menu->all(['main_id'=>0]);
+                    foreach($rows as $row){
+                        
                     ?>
-                    <tr>                       
+                    <tr>
                         <td>
-                            <input type="text" name="text[]"  value="<?=$row['text'];?>">
+                            <input type="text" name="text[]" value="<?=$row['text'];?>">    
                         </td>
                         <td>
-                            <input type="text" name="href[]" value="<?=$row['href'];?>" >    
+                            <input type="text" name="href[]" value="<?=$row['href'];?>">
                         </td>
-                        <td></td>
+                        <td><?=$Menu->count(['main_id'=>$row['id']]);?></td>
                         <td>
-                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>    
-                        </td> 
-                        <td>
-                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">     
+                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
                         </td>
                         <td>
-                            <input type="button" 
-                                value="編輯次選單"
+                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        </td>
+                        <td>
+                            <input type="button"
+                                  value="編輯次選單"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/submenu.php?id=<?=$row['id'];?>&#39;)">
-                        </td>                    
+                        </td>
                         <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     </tr>
                     <?php
+                        
                     }
-                // }
                     ?>
                 </tbody>
             </table>
