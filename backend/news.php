a6ca3da3da57336aa5ed2f0025d3b63215ed3b79
sandy0v0 <be0324ss@gmail.com>
2024-12-09 14:52:11 +0800

    完成後台的分頁功能

--- backend/news.php
+++ backend/news.php
@@ -22,8 +22,16 @@
                         <td width="10%">刪除</td>
                     </tr>
                     <?php
+
+                    $div=4;
+                    $total=$News->count();
+                    $pages=ceil($total/$div);
+                    $now=$_GET['p']??1;
+                    $start=($now-1)*$div;
+
+                    $rows=$News->all(" limit $start,$div");
+
                     // $db=ucfirst($do); (如果你想要自己在做大小寫的話)
-                    $rows=$News->all();
                     foreach($rows as $row){                  
                     ?>
                     <tr>                       
@@ -43,6 +51,29 @@
                     ?>
                 </tbody>
             </table>
+
+            <div class="cent">
+            <?php
+
+                if(($now-1)>0 ){ 
+                    // [&lt;]就是 [<]
+                    $prev=$now-1;
+                    echo "<a href='?do=$do&p=$prev'> < </a>";
+                }
+                for($i=1;$i<=$pages;$i++){
+                    echo "<a href='?do=$do&p=$i'> ";
+                    echo $i;
+                    echo "</a>";
+                }
+
+                if(($now+1)<=$pages){ 
+                    // [&lt;]就是 [<]
+                    $next=$now+1;
+                    echo "<a href='?do=$do&p=$next'> > </a>";
+                }
+
+            ?>
+            </div>
 
             <!-- [#39;] 代表['] -->
             <table style="margin-top:40px; width:70%;">
