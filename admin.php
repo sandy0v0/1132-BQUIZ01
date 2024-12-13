7c44543a8a316c9529d1f4a87094068f515310c6
sandy0v0 <be0324ss@gmail.com>
2024-12-13 10:27:56 +0800

    使用彈出視窗完成進站人數功能

--- admin.php
+++ admin.php
@@ -48,7 +48,8 @@
                         <div class="mainmu">
                             校園映象資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=total">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="#" 
+                    onclick="op('#cover','#cvr','./modal/view.php')">
                         <div class="mainmu">
                             進站總人數管理 </div>
                     </a>
