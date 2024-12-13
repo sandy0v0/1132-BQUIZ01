47fed1876e420df220fc1768cea57764f9b558a0
sandy0v0 <be0324ss@gmail.com>
2024-12-13 16:02:31 +0800

    完成動畫輪播功能

--- front/main.php
+++ front/main.php
@@ -13,27 +13,43 @@
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
-    <script>
-    var lin = new Array();
-    var now = 0;
-    if (lin.length > 1) {
-        setInterval("ww()", 3000);
-        now = 1;
-    }
-
-    function ww() {
-        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-        //$("#mwww").attr("src",lin[now])
-        now++;
-        if (now >= lin.length)
-            now = 0;
-    }
-    </script>
+    
     <div style="width:100%; padding:2px; height:290px;">
         <div id="mwww" loop="true" style="width:100%; height:100%;">
             <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
         </div>
     </div>
+    
+    <script>
+    var lin = new Array();
+    // 抓 lin 裡面的動畫資料去撥放
+    // lin=['upload/01C01.gif','upload/01C02.gif'];
+        <?php
+        $mvs=$Mvim->all(['sh'=>1]);
+        foreach($mvs as $mv){
+            echo "lin.push('upload/{$mv['img']}');";
+        }
+        ?>
+    var now = 0;
+    if (lin.length > 1) {
+        // 每間隔3秒，執行ww()程式
+        // setInterval 是非同步的機制
+        setInterval("ww()", 3000);
+        // now = 1;
+    }
+
+    function ww() {
+        // console.log("HI");
+        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+        //$("#mwww").attr("src",lin[now])
+        now++;
+        if (now >= lin.length)       
+            now = 0;
+    }
+
+    ww();
+    </script>
+
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
