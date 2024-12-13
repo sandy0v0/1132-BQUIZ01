c51ce68654e25eb5056f89b9508781661eb91a47
sandy0v0 <be0324ss@gmail.com>
2024-12-13 14:20:30 +0800

    完成動態文字廣告跑馬燈顯示

--- front/main.php
+++ front/main.php
@@ -1,6 +1,15 @@
 <div class="di"
     style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
     <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+        <?php
+        $ads=$Ad->all(['sh'=>1]);
+        foreach($ads as $ad){
+            echo $ad['text'];
+            // echo "&nbsp;&nbsp;&nbsp;"
+            echo str_repeat("&nbsp;",4) ;
+        }
+        // 這段沒有成功，可刪-> echo q("select group_concat('&nbsp;&nbsp;&nbsp;&nbsp;',`text`) from `ads` where `sh`=1 group by id")[0];
+        ?>
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
