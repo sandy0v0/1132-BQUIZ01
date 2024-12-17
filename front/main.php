50e0e46eb07283f74907f5117754bb4fb12e1dc4
sandy0v0 <be0324ss@gmail.com>
2024-12-17 10:25:49 +0800

    在首頁顯示最新消息資料及more文字

--- front/main.php
+++ front/main.php
@@ -34,6 +34,7 @@
     if (lin.length > 1) {
         // 每間隔3秒，執行ww()程式
         // setInterval 是非同步的機制
+        // 如果資料庫沒有設定顯示的圖片，就顯示沒有可顯示的動畫圖片資料，請確認動畫圖片顯示的判斷
         setInterval("ww()", 3000);
         ww();
         // now = 1;
@@ -57,13 +58,48 @@
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+                if($News->count(['sh'=>1])>5){
+                    echo "<a href='index.php?do=news' style='float:right'>";
+                    echo "More...";
+                    echo "</a>" ;
+                }
+            ?>
+
+        <!-- ul裡面要放li，代表有序的清單 -->
         </span>
-        <ul class="ssaa" style="list-style-type:decimal;">
-        </ul>
+        <!-- <ol class="ssaa" style="list-style-type:decimal;" > -->
+        <ol class="ssaa" style="list-style-type:decimal;">
+        
+            <?php
+                $all_news=$News->all(['sh'=>1]," limit 5");
+                foreach($all_news as $n){
+                    echo "<li>";
+                    echo mb_substr($n['text'],0,20);
+                        echo "<span class='all' style='display:none'>";
+                        echo $n['text'];
+                        echo "</span>";
+                    echo "</li>";
+                }
+            ?>
+
+        </ol>
+        <!-- position: absolute　絕對定位 -->
+        <!-- 先不顯示，滑鼠移過之後再顯示，此為彈出視窗 -->
         <div id="altt"
             style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
         </div>
-        <script>
+
+        <!-- ssaa li 因為他中間顯示空白，代表ssaa在li的上層-->
+        <!-- 原生jQ的寫法，代表滑鼠滑到該位置跟離開位置的動作，改成JS -->
+        <!-- $(this)代表 $(".ssaa li")的li，表示每個li裡面都有東西，裡面有個all，取得裡面全部的html的內容，並前後再加上</pre>-->
+        <!-- 原本是display: none (隱藏)，當我滑鼠移到該位置時，做完以上動作後，把它顯示出來 -->
+        <!-- 在$(".ssaa li").mouseout中，表示滑鼠離開的時候，又消失 -->
+
+        <!-- 在 JavaScript 中，substr的語法，str.substr(start, length);　-->
+        <!-- str.substr( 1, 10 );  ( 從字串中的哪一個位置開始提取 , 要提取的字的長度有幾個字 )-->
+
+        <script>  
         $(".ssaa li").hover(
             function() {
                 $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
--- front/news.php
+++ front/news.php
@@ -4,10 +4,63 @@
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
-    <div style="text-align:center;">
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
-    </div>
+    <h3>更多最新消息資料區</h3>
+    <hr>
+    <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+
+
+
+
+
+
+
+
+
+
+                $all_news=$News->all(['sh'=>1]," limit 5");
+                foreach($rows as $row){
+                    echo "<li>";
+                    echo mb_substr($n['text'],0,20);
+                    echo "<span class='all' style='display: none'>";
+                    echo $n['text'];
+                    echo "</span>";
+                    echo "</li>";
+                }
+            ?>
+
+<div class="cent">
+            <?php
+
+                if(($now-1)>0 ){ 
+                    // [&lt;]就是 [<]
+                    $prev=$now-1;
+                    echo "<a href='?do=$do&p=$prev'> < </a>";
+                }
+                for($i=1;$i<=$pages;$i++){
+                    echo "<a href='?do=$do&p=$i'> ";
+                    echo $i;
+                    echo "</a>";
+                }
+
+                if(($now+1)<=$pages){ 
+                    // [&lt;]就是 [<]
+                    $next=$now+1;
+                    echo "<a href='?do=$do&p=$next'> > </a>";
+                }
+
+            ?>
+            </div>
+
+
+
+
+
+
+
+
+
+
 </div>
 <div id="alt"
     style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
